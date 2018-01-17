b183372655ebc413c6c9b642651519618f89d3df
Aditya Goel <aditya.goel@timesgroup.com>
2018-01-17 14:07:39 +0530

    Updated Code

--- ArrayBasedStack.java
+++ ArrayBasedStack.java
@@ -1,63 +1,54 @@
-package com.aditya.mlabs.structures;
+package com.aditya.mlabs.structures.ArrayBasedStack;
 
 public class ArrayBasedStack implements Stack {
 
-	
 	public static final int CAPACITY = 1024;		// Default Capacity of Stack. 
-	private int N;									// Maximum Capacity of Stack.
 	private Object S[];								// S holds the elements of Stack. 
-	private int t = -1;								// The Top element of Stack. 
+	private int t = -1;								// The Top element index of Stack. 
 	
 	public ArrayBasedStack() {
-		this(CAPACITY);
+		S = new Object[CAPACITY];
 	}
 	
-	public ArrayBasedStack(int cap) {
-		N = cap;
-		S = new Object[N];
-	}
-
-	@Override
-	public int size() {
-		// Returns the Current Size of Stack.
-		return (t+1);
-	}
-
-	@Override
-	public boolean isEmpty() {
-		// Returns true if Stack is empty.
-		return (t < 0);
-	}
-
-	@Override
 	public void push(Object element) throws StackFullException {
-		// Push a new element on the Stack.
-		if (size() == N){
-			throw new StackFullException("Stack overflow");
+		if((t+1) >= (CAPACITY -1)){
+			throw new StackFullException("Stack is full");
+		} else {
+			S[t+1] = element;
+			t++;
 		}
-		S[++t] = element;
+		
 	}
 
-	@Override
-	public Object top() throws StackEmptyException {
-		// Fetches Top Stack Element
-		if(isEmpty()){
-			throw new StackEmptyException("Stack is empty");
+	public Object pop() {
+		if(t == -1){
+			throw new StackEmptyException("Stack is Empty");
+		} else {
+			Object popedElement = S[t];
+			S[t] = null;
+			t --;
+			return popedElement;
 		}
-		return S[t];
 	}
 	
-	@Override
-	public Object pop() throws StackEmptyException {
-		// Pop off the Top Stack element
-		Object choppedElement;
-		if(isEmpty()){
-			throw new StackEmptyException("Stack is empty");
-		}
-		choppedElement = S[t];
-		S[t] = null; 		// Dereference the Top Element. 
-		t--;				// Decrement top.
-		return choppedElement;
+	public int size() {
+		return t+1;
 	}
 
+	public boolean isEmpty() {
+		return (t == -1);
+	}
+
+	public Object top() throws StackEmptyException {
+		if(t == -1){
+			throw new StackEmptyException("Stack is Empty");
+		} else {
+			return S[t];
+		}
+	}
+
+
+	
+	
+	
 }
